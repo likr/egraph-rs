72cacd9699041229bde1e3713939125d191cd99e
Yosuke ONOUE <onoue@likr-lab.com>
2018-08-26 16:40:51 +0900

    [js-example] Add edge bundling options form

--- egraph-js-examples/src/pages/example-group-in-a-box.js
+++ egraph-js-examples/src/pages/example-group-in-a-box.js
@@ -21,7 +21,7 @@ const countGroups = (nodes) => {
   return groups
 }
 
-const layout = (Module, graph, data) => {
+const layout = (Module, graph, data, options) => {
   const width = 800
   const height = 600
   const allocator = new Allocator(Module)
@@ -55,6 +55,11 @@ const layout = (Module, graph, data) => {
   simulation.start(graph)
 
   const edgeBundling = new EdgeBundling(Module)
+  edgeBundling.cycles = options.cycles
+  edgeBundling.s0 = options.s0
+  edgeBundling.i0 = options.i0
+  edgeBundling.sStep = options.sStep
+  edgeBundling.iStep = options.iStep
   const lines = edgeBundling.call(graph)
 
   tiles.forEach((tile, i) => {
@@ -74,56 +79,116 @@ const layout = (Module, graph, data) => {
   })
 }
 
-const draw = (renderer, data) => {
-  const color = d3.scaleOrdinal(d3.schemeCategory10)
-  for (const node of data.nodes) {
-    node.fillColor = color(node.group)
-  }
-  for (const link of data.links) {
-    link.strokeWidth = Math.sqrt(link.value)
-  }
-  renderer.load(data)
-  renderer.center()
-}
-
 export class ExampleGroupInABox extends React.Component {
   componentDidMount () {
-    loadModule().then(({Module}) => {
-      window.fetch('/data/miserables.json')
-        .then((response) => response.json())
-        .then((data) => {
-          const graph = new Graph(Module)
-          data.nodes.forEach(() => {
-            graph.addNode()
-          })
-          for (const {source, target} of data.links) {
-            graph.addEdge(source, target)
-          }
-          layout(Module, graph, data)
-          draw(this.refs.renderer, data)
+    window.fetch('/data/miserables.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const color = d3.scaleOrdinal(d3.schemeCategory10)
+        for (const node of data.nodes) {
+          node.fillColor = color(node.group)
+        }
+        for (const link of data.links) {
+          link.strokeWidth = Math.sqrt(link.value)
+        }
+
+        this.data = data
+        this.layout().then(() => {
+          this.refs.renderer.center()
         })
-    })
+      })
   }
 
   render () {
-    return <Wrapper onResize={this.handleResize.bind(this)}>
-      <eg-renderer
-        ref='renderer'
-        default-node-width='10'
-        default-node-height='10'
-        default-node-stroke-color='#fff'
-        default-node-stroke-width='1.5'
-        default-node-type='circle'
-        default-link-stroke-color='#999'
-        default-link-stroke-opacity='0.6'
-        node-label-property='name'
-        no-auto-centering
-      />
-    </Wrapper>
+    return <div>
+      <div>
+        <Wrapper onResize={this.handleResize.bind(this)}>
+          <eg-renderer
+            ref='renderer'
+            default-node-width='10'
+            default-node-height='10'
+            default-node-stroke-color='#fff'
+            default-node-stroke-width='1.5'
+            default-node-type='circle'
+            default-link-stroke-color='#999'
+            default-link-stroke-opacity='0.6'
+            node-label-property='name'
+            no-auto-centering
+          />
+        </Wrapper>
+      </div>
+      <div>
+        <h3 className='title'>Edge Bundling Options</h3>
+        <form onSubmit={this.handleSubmitEdgeBundlingOptions.bind(this)}>
+          <div className='field'>
+            <label className='label'>Cycle</label>
+            <div className='control'>
+              <input ref='cycles' className='input' type='number' min='1' step='1' defaultValue='6' />
+            </div>
+          </div>
+          <div className='field'>
+            <label className='label'>S0</label>
+            <div className='control'>
+              <input ref='s0' className='input' type='number' min='0' step='0.01' defaultValue='0.1' />
+            </div>
+          </div>
+          <div className='field'>
+            <label className='label'>S Step</label>
+            <div className='control'>
+              <input ref='sStep' className='input' type='number' min='Step' step='0.01' defaultValue='0.5' />
+            </div>
+          </div>
+          <div className='field'>
+            <label className='label'>I0</label>
+            <div className='control'>
+              <input ref='i0' className='input' type='number' min='0' step='1' defaultValue='90' />
+            </div>
+          </div>
+          <div className='field'>
+            <label className='label'>I Step</label>
+            <div className='control'>
+              <input ref='iStep' className='input' type='number' min='Step' step='0.01' defaultValue='0.6' />
+            </div>
+          </div>
+          <div className='field'>
+            <div className='control'>
+              <button className='button' type='submit'>Update</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   }
 
   handleResize (width, height) {
     this.refs.renderer.width = width
     this.refs.renderer.height = height
+  }
+
+  handleSubmitEdgeBundlingOptions (event) {
+    event.preventDefault()
+    this.layout()
+  }
+
+  layout () {
+    return loadModule().then(({Module}) => {
+      const graph = new Graph(Module)
+      this.data.nodes.forEach(() => {
+        graph.addNode()
+      })
+      for (const {source, target} of this.data.links) {
+        graph.addEdge(source, target)
+      }
+
+      layout(Module, graph, this.data, {
+        cycles: +this.refs.cycles.value,
+        s0: +this.refs.s0.value,
+        i0: +this.refs.i0.value,
+        sStep: +this.refs.sStep.value,
+        iStep: +this.refs.iStep.value
+      })
+
+      this.refs.renderer.load(this.data)
+    })
   }
 }
