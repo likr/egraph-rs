4999fc00476412282fd767dd7a1dc4d93088fad5
Yosuke ONOUE <onoue@likr-lab.com>
2018-09-12 18:08:28 +0900

    [layout] WIP fm3 layout

--- egraph/src/layout/fm3.rs
+++ egraph/src/layout/fm3.rs
@@ -3,6 +3,9 @@ extern crate petgraph;
 
 use petgraph::{Graph, EdgeType};
 use petgraph::graph::{IndexType, NodeIndex};
+use ::layout::force_directed::{initial_placement, initial_links};
+use ::layout::force_directed::force::{Force, CenterForce, LinkForce, ManyBodyForce};
+use ::layout::force_directed::simulation::start_simulation;
 
 fn shuffled_nodes<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E, Ty, Ix>) -> Vec<NodeIndex<Ix>> {
     let mut nodes = graph.node_indices().collect::<Vec<_>>();
@@ -15,32 +18,49 @@ fn shuffled_nodes<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E, Ty, Ix>
 }
 
 pub struct Node {
+    pub x: f64,
+    pub y: f64,
+    pub radius: f64,
     pub group: usize,
-    pub parent_index: usize,
+    pub parent: usize,
     pub node_type: Option<NodeType>,
 }
 
+impl Node {
+    fn new() -> Node {
+        Node {
+            x: 0.,
+            y: 0.,
+            radius: 0.,
+            group: 0,
+            parent: 0,
+            node_type: None,
+        }
+    }
+}
+
 pub struct Edge {
-    pub length: f32,
+    pub length: f64,
     pub count: usize,
 }
 
+impl Edge {
+    fn new() -> Edge {
+        Edge {
+            length: 0.,
+            count: 0,
+        }
+    }
+}
+
 pub enum NodeType {
-    Unknown,
     SunNode,
     PlanetNode,
     MoonNode,
-    PMNode,
 }
 
-fn solar_system_partition<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E, Ty, Ix>) -> Vec<usize> {
+fn solar_system_partition(graph: &mut Graph<Node, Edge>) {
     let nodes = shuffled_nodes(graph);
-    let mut groups = graph.node_indices()
-        .map(|_| 0)
-        .collect::<Vec<_>>();
-    let mut node_types = graph.node_indices()
-        .map(|_| NodeType::Unknown)
-        .collect::<Vec<_>>();
     let mut visited = graph.node_indices()
         .map(|_| false)
         .collect::<Vec<_>>();
@@ -49,82 +69,202 @@ fn solar_system_partition<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E,
         if visited[s.index()] {
             continue;
         }
-        groups[s.index()] = i;
-        node_types[s.index()] = NodeType::SunNode;
+        {
+            let s_node = graph.node_weight_mut(s).unwrap();
+            s_node.group = i;
+            s_node.parent = s.index();
+            s_node.node_type = Some(NodeType::SunNode);
+        }
         visited[s.index()] = true;
-        for p in graph.neighbors_undirected(s) {
+        let mut s_neighbors = graph.neighbors_undirected(s).detach();
+        while let Some(p) = s_neighbors.next_node(graph) {
             if visited[p.index()] {
                 continue;
             }
-            groups[p.index()] = i;
-            node_types[p.index()] = NodeType::PlanetNode;
-            visited[p.index()] = true;
-            let mut has_moon_node = false;
-            for m in graph.neighbors_undirected(p) {
-                if !visited[m.index()] {
-                    groups[m.index()] = i;
-                    node_types[m.index()] = NodeType::MoonNode;
-                    visited[m.index()] = true;
-                    has_moon_node = true;
-                }
+            {
+                let p_node = graph.node_weight_mut(p).unwrap();
+                p_node.group = i;
+                p_node.parent = s.index();
+                p_node.node_type = Some(NodeType::PlanetNode);
             }
-            if has_moon_node {
-                node_types[p.index()] = NodeType::PMNode;
+            visited[p.index()] = true;
+            let mut p_neighbors = graph.neighbors_undirected(p).detach();
+            while let Some(m) = p_neighbors.next_node(graph) {
+                if !visited[m.index()] {
+                    {
+                        let m_node = graph.node_weight_mut(m).unwrap();
+                        m_node.group = i;
+                        m_node.parent = p.index();
+                        m_node.node_type = Some(NodeType::MoonNode);
+                    }
+                    visited[m.index()] = true;
+                }
             }
         }
         i += 1;
     }
-    groups
 }
 
-fn collaps<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E, Ty, Ix>, groups: &Vec<usize>) -> Graph<Node, Edge> {
+fn edge_length(graph: &Graph<Node, Edge>, u: NodeIndex, v: NodeIndex) -> f64 {
+    let (e, _) = graph.find_edge_undirected(u, v).unwrap();
+    graph.edge_weight(e).unwrap().length
+}
+
+fn collapse(graph: &mut Graph<Node, Edge>) -> Graph<Node, Edge> {
     let mut shrinked_graph : Graph<Node, Edge> = Graph::new();
-    let num_groups = groups.iter().max().unwrap() + 1;
+    let num_groups = graph.raw_nodes().iter().map(|node| node.weight.group).max().unwrap() + 1;
     for _ in 0..num_groups {
-        shrinked_graph.add_node(Node {group: 0, parent_index: 0, node_type: None});
+        shrinked_graph.add_node(Node::new());
     }
     for e in graph.edge_indices() {
         let (u0, v0) = graph.edge_endpoints(e).unwrap();
-        let u1 = NodeIndex::new(groups[u0.index()]);
-        let v1 = NodeIndex::new(groups[v0.index()]);
+        if graph[u0].group == graph[v0].group {
+            continue;
+        }
+        let e_u_length = match &graph[u0].node_type {
+            Some(NodeType::PlanetNode) => {
+                let s0 = NodeIndex::new(graph.node_weight(u0).unwrap().parent);
+                edge_length(graph, u0, s0)
+            },
+            Some(NodeType::MoonNode) => {
+                let p0 = NodeIndex::new(graph.node_weight(u0).unwrap().parent);
+                let s0 = NodeIndex::new(graph.node_weight(p0).unwrap().parent);
+                edge_length(graph, u0, p0) + edge_length(graph, p0, s0)
+            },
+            _ => 0.,
+        };
+        let e_v_length = match &graph[v0].node_type {
+            Some(NodeType::PlanetNode) => {
+                let t0 = NodeIndex::new(graph.node_weight(v0).unwrap().parent);
+                edge_length(graph, v0, t0)
+            },
+            Some(NodeType::MoonNode) => {
+                let q0 = NodeIndex::new(graph.node_weight(v0).unwrap().parent);
+                let t0 = NodeIndex::new(graph.node_weight(q0).unwrap().parent);
+                edge_length(graph, v0, q0) + edge_length(graph, q0, t0)
+            },
+            _ => 0.,
+        };
+        graph[u0].radius = e_u_length;
+        graph[v0].radius = e_v_length;
+        let e_length = edge_length(graph, u0, v0);
+        let p_length = e_u_length + e_length + e_v_length;
+        let u1 = NodeIndex::new(graph.node_weight(u0).unwrap().group);
+        let v1 = NodeIndex::new(graph.node_weight(v0).unwrap().group);
         match shrinked_graph.find_edge_undirected(u1, v1) {
             Some((e1, _)) => {
                 let edge = shrinked_graph.edge_weight_mut(e1).unwrap();
+                edge.length += p_length;
                 edge.count += 1;
             }
             None => {
-                shrinked_graph.add_edge(u1, v1, Edge {length: 1.0, count: 1});
+                let mut edge = Edge::new();
+                edge.length = p_length;
+                shrinked_graph.add_edge(u1, v1, edge);
             }
         }
     }
     for e in shrinked_graph.edge_indices() {
         let edge = shrinked_graph.edge_weight_mut(e).unwrap();
-        edge.length /= edge.count as f32;
+        edge.length /= edge.count as f64;
     }
     shrinked_graph
 }
 
-pub fn fm3<N, E, Ty: EdgeType, Ix: IndexType>(graph: &Graph<N, E, Ty, Ix>) {
-    let groups = solar_system_partition(graph);
-    let collapsed_graph = collaps(graph, &groups);
-    println!("{} {}", collapsed_graph.node_count(), collapsed_graph.edge_count());
+fn expand(graph0: &mut Graph<Node, Edge>, graph1: &Graph<Node, Edge>) {
+    for u in graph0.node_indices() {
+        let mut x = 0.;
+        let mut y = 0.;
+        let mut count = 0;
+        for v in graph0.neighbors_undirected(u) {
+            if graph0[u].group == graph0[v].group {
+                continue;
+            }
+            let s1 = NodeIndex::new(graph0[u].group);
+            let t1 = NodeIndex::new(graph0[v].group);
+            let s1_x = graph1[s1].x;
+            let s1_y = graph1[s1].y;
+            let t1_x = graph1[t1].x;
+            let t1_y = graph1[t1].y;
+            let scale = graph0[u].radius / edge_length(graph1, s1, t1);
+            x += (t1_x - s1_x) * scale + s1_x;
+            y += (t1_y - s1_y) * scale + s1_y;
+            count += 1;
+        }
+        graph0[u].x = x / count as f64;
+        graph0[u].y = y / count as f64;
+    }
+}
+
+fn layout(graph: &mut Graph<Node, Edge>) {
+    let links = initial_links(graph);
+    let mut forces : Vec<Box<Force>> = Vec::new();
+    forces.push(Box::new(ManyBodyForce::new()));
+    forces.push(Box::new(LinkForce::new_with_links(links)));
+    forces.push(Box::new(CenterForce::new()));
+    let mut points = initial_placement(graph.node_count());
+    start_simulation(&mut points, &forces);
+    for (u, point) in graph.node_indices().zip(points) {
+        graph[u].x = point.x as f64;
+        graph[u].y = point.y as f64;
+    }
+}
+
+fn layout_with_initial_placement(graph: &mut Graph<Node, Edge>) {
+    let links = initial_links(graph);
+    let mut forces : Vec<Box<Force>> = Vec::new();
+    forces.push(Box::new(ManyBodyForce::new()));
+    forces.push(Box::new(LinkForce::new_with_links(links)));
+    forces.push(Box::new(CenterForce::new()));
+    let mut points = initial_placement(graph.node_count());
+    for (u, point) in graph.node_indices().zip(points.iter_mut()) {
+        point.x = graph[u].x as f32;
+        point.y = graph[u].y as f32;
+    }
+    start_simulation(&mut points, &forces);
+    for (u, point) in graph.node_indices().zip(points) {
+        graph[u].x = point.x as f64;
+        graph[u].y = point.y as f64;
+    }
+}
+
+pub fn fm3(graph: Graph<Node, Edge>) {
+    let mut shrinked_graphs = Vec::new();
+    let mut g0 = graph;
+    while g0.node_count() > 100 {
+        println!("{} {}", g0.node_count(), g0.edge_count());
+        solar_system_partition(&mut g0);
+        let g1 = collapse(&mut g0);
+        shrinked_graphs.push(g0);
+        g0 = g1;
+    }
+    let mut gk = g0;
+    println!("{} {}", gk.node_count(), gk.edge_count());
+    layout(&mut gk);
+    for i in 1..shrinked_graphs.len() {
+        // let mut g0 = &mut shrinked_graphs[i - 1];
+        // let g1 = &shrinked_graphs[i];
+        // expand(&mut g0, &g1);
+        // layout_with_initial_placement(&mut g0);
+    }
+
 }
 
 #[test]
 fn test_fm3() {
-    let rows = 10;
-    let cols = 10;
-    let mut graph = Graph::new_undirected();
-    let nodes = (0..rows * cols).map(|_| graph.add_node(Node {group: 0, parent_index: 0, node_type: None})).collect::<Vec<_>>();
+    let rows = 100;
+    let cols = 100;
+    let mut graph = Graph::new();
+    let nodes = (0..rows * cols).map(|_| graph.add_node(Node::new())).collect::<Vec<_>>();
     for i in 0..rows {
         for j in 0..cols {
             if i != rows - 1 {
-                graph.add_edge(nodes[i * cols + j], nodes[(i + 1) * cols + j], Edge {length: 1.0, count: 1});
+                graph.add_edge(nodes[i * cols + j], nodes[(i + 1) * cols + j], Edge::new());
             }
             if j != cols - 1 {
-                graph.add_edge(nodes[i * cols + j], nodes[i * cols + j + 1], Edge {length: 1.0, count: 1});
+                graph.add_edge(nodes[i * cols + j], nodes[i * cols + j + 1], Edge::new());
             }
         }
     }
-    fm3(&graph);
+    fm3(graph);
 }
