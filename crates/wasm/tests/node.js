99dccdf674adcda276be6636e40d3e79257745a2
Yosuke Onoue <onoue@likr-lab.com>
2021-06-30 17:54:38 +0900

    [wasm] add tests for force-simulation

--- crates/wasm/tests/node.js
+++ crates/wasm/tests/node.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const wasm = require("wasm-bindgen-test");
 
-exports.testConstructGraph = function (data) {
+function constructGraph(data) {
   const { Graph } = wasm;
   const graph = new Graph();
   const indices = new Map();
@@ -12,6 +12,91 @@ exports.testConstructGraph = function (data) {
     const { source, target } = link;
     graph.addEdge(indices.get(source), indices.get(target), link);
   }
+  return graph;
+}
+
+function runSimulation(graph, forces) {
+  const { Simulation, initialPlacement } = wasm;
+  const initialCoordinates = initialPlacement(graph);
+  const simulation = new Simulation(graph, (u) => initialCoordinates[u]);
+  return simulation.run(forces);
+}
+
+function checkSimulation(graph, forces) {
+  const coordinates = runSimulation(graph, forces);
+  for (const u of graph.nodeIndices()) {
+    assert(Number.isFinite(coordinates[u][0]));
+    assert(Number.isFinite(coordinates[u][1]));
+  }
+}
+
+exports.testConstructGraph = function (data) {
+  const graph = constructGraph(data);
   assert.strictEqual(graph.nodeCount(), data.nodes.length);
   assert.strictEqual(graph.edgeCount(), data.links.length);
+};
+
+exports.testSimulation = function (data) {
+  const { Simulation, initialPlacement } = wasm;
+  const graph = constructGraph(data);
+  const initialCoordinates = initialPlacement(graph);
+  const simulation = new Simulation(graph, (u) => initialCoordinates[u]);
+  const coordinates = simulation.run([]);
+  for (const u of graph.nodeIndices()) {
+    assert.strictEqual(initialCoordinates[u][0], coordinates[u][0]);
+    assert.strictEqual(initialCoordinates[u][1], coordinates[u][1]);
+  }
+};
+
+exports.testCenterForce = function (data) {
+  const { CenterForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [new CenterForce()]);
+};
+
+exports.testCollideForce = function (data) {
+  const { CollideForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [
+    new CollideForce(graph, () => ({ radius: 10 }), {
+      strength: 0.1,
+      iterations: 1,
+    }),
+  ]);
+};
+
+exports.testLinkForce = function (data) {
+  const { LinkForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [new LinkForce(graph)]);
+  checkSimulation(graph, [new LinkForce(graph, () => ({}))]);
+  checkSimulation(graph, [new LinkForce(graph, () => ({ strength: 0.5 }))]);
+  checkSimulation(graph, [new LinkForce(graph, () => ({ distance: 30 }))]);
+  checkSimulation(graph, [
+    new LinkForce(graph, () => ({ distance: 30, strength: 0.5 })),
+  ]);
+};
+
+exports.testManyBodyForce = function (data) {
+  const { ManyBodyForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [new ManyBodyForce(graph)]);
+  checkSimulation(graph, [new ManyBodyForce(graph, () => ({}))]);
+  checkSimulation(graph, [new ManyBodyForce(graph, () => ({ strength: 30 }))]);
+};
+
+exports.testPositionForce = function (data) {
+  const { PositionForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [
+    new PositionForce(graph, () => ({ strength: 0.1, x: 0, y: 0 })),
+  ]);
+};
+
+exports.testRadialForce = function (data) {
+  const { RadialForce } = wasm;
+  const graph = constructGraph(data);
+  checkSimulation(graph, [
+    new RadialForce(graph, () => ({ strength: 0.1, radius: 100, x: 0, y: 0 })),
+  ]);
 };
--- crates/wasm/tests/node.rs
+++ crates/wasm/tests/node.rs
@@ -23,16 +23,75 @@ struct GraphData {
 	links: Vec<LinkData>,
 }
 
+fn example_data() -> JsValue {
+	let s = include_str!("./miserables.json");
+	let data = serde_json::from_str::<GraphData>(&s).unwrap();
+	JsValue::from_serde(&data).ok().unwrap()
+}
+
 #[wasm_bindgen(module = "tests/node.js")]
 extern "C" {
 	#[wasm_bindgen(js_name = "testConstructGraph")]
 	fn test_construct_graph(data: JsValue);
+	#[wasm_bindgen(js_name = "testSimulation")]
+	fn test_simulation(data: JsValue);
+	#[wasm_bindgen(js_name = "testCenterForce")]
+	fn test_center_force(data: JsValue);
+	#[wasm_bindgen(js_name = "testCollideForce")]
+	fn test_collide_force(data: JsValue);
+	#[wasm_bindgen(js_name = "testLinkForce")]
+	fn test_link_force(data: JsValue);
+	#[wasm_bindgen(js_name = "testManyBodyForce")]
+	fn test_many_body_force(data: JsValue);
+	#[wasm_bindgen(js_name = "testPositionForce")]
+	fn test_position_force(data: JsValue);
+	#[wasm_bindgen(js_name = "testRadialForce")]
+	fn test_radial_force(data: JsValue);
 }
 
 #[wasm_bindgen_test]
 pub fn construct_graph() {
-	let s = include_str!("./miserables.json");
-	let data = serde_json::from_str::<GraphData>(&s).unwrap();
-	let data = JsValue::from_serde(&data).ok().unwrap();
+	let data = example_data();
 	test_construct_graph(data);
+}
+
+#[wasm_bindgen_test]
+pub fn simulation() {
+	let data = example_data();
+	test_simulation(data);
+}
+
+#[wasm_bindgen_test]
+pub fn center_force() {
+	let data = example_data();
+	test_center_force(data);
+}
+#[wasm_bindgen_test]
+pub fn collide_force() {
+	let data = example_data();
+	test_collide_force(data);
+}
+
+#[wasm_bindgen_test]
+pub fn link_force() {
+	let data = example_data();
+	test_link_force(data);
+}
+
+#[wasm_bindgen_test]
+pub fn many_body_force() {
+	let data = example_data();
+	test_many_body_force(data);
+}
+
+#[wasm_bindgen_test]
+pub fn position_force() {
+	let data = example_data();
+	test_position_force(data);
+}
+
+#[wasm_bindgen_test]
+pub fn radial_force() {
+	let data = example_data();
+	test_radial_force(data);
 }
