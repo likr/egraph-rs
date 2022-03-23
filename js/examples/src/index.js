89cd7c4431455363f268d683cfb9ad8d1ff409fd
Yosuke Onoue <onoue@likr-lab.com>
2022-03-23 17:16:00 +0900

    avoid top level await

--- js/examples/src/index.js
+++ js/examples/src/index.js
@@ -3,5 +3,6 @@ import "./styles.css";
 import init from "egraph/dist/web/egraph_wasm";
 import wasm from "egraph/dist/web/egraph_wasm_bg.wasm?url";
 
-await init(wasm);
-import("./App");
+init(wasm).then(() => {
+  import("./App");
+});
