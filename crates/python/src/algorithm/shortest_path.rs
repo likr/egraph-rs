31b35eed19eb4491b337622e0259bead3e0309a2
Yosuke Onoue <onoue@likr-lab.com>
2023-02-24 23:48:58 +0900

    feat(python): add all sources bfs and dijkstra

--- crates/python/src/algorithm/shortest_path.rs
+++ crates/python/src/algorithm/shortest_path.rs
@@ -1,8 +1,27 @@
 use crate::{distance_matrix::PyDistanceMatrix, graph::PyGraph};
 use petgraph::visit::EdgeRef;
-use petgraph_algorithm_shortest_path::warshall_floyd;
+use petgraph_algorithm_shortest_path::{all_sources_bfs, all_sources_dijkstra, warshall_floyd};
 use pyo3::prelude::*;
 use std::collections::HashMap;
+
+#[pyfunction]
+#[pyo3(name = "all_sources_bfs")]
+fn py_all_sources_bfs(graph: &PyGraph, unit_edge_length: f32) -> PyDistanceMatrix {
+    let distance_matrix = all_sources_bfs(graph.graph(), unit_edge_length);
+    PyDistanceMatrix::new(distance_matrix)
+}
+
+#[pyfunction]
+#[pyo3(name = "all_sources_dijkstra")]
+fn py_all_sources_dijkstra(graph: &PyGraph, f: &PyAny) -> PyDistanceMatrix {
+    let mut distance = HashMap::new();
+    for e in graph.graph().edge_indices() {
+        let v = f.call1((e.index(),)).unwrap().extract().unwrap();
+        distance.insert(e, v);
+    }
+    let distance_matrix = all_sources_dijkstra(graph.graph(), &mut |e| distance[&e.id()]);
+    PyDistanceMatrix::new(distance_matrix)
+}
 
 #[pyfunction]
 #[pyo3(name = "warshall_floyd")]
@@ -17,6 +36,8 @@ fn py_warshall_floyd(graph: &PyGraph, f: &PyAny) -> PyDistanceMatrix {
 }
 
 pub fn register(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_all_sources_bfs, m)?)?;
+    m.add_function(wrap_pyfunction!(py_all_sources_dijkstra, m)?)?;
     m.add_function(wrap_pyfunction!(py_warshall_floyd, m)?)?;
     Ok(())
 }
