1e6bfbd0b721001d018aba5b0c946411ced0ccc8
Yosuke Onoue <onoue@likr-lab.com>
2023-03-02 12:39:06 +0900

    feat(python): add getter and setter for distance matrix

--- crates/python/src/distance_matrix.rs
+++ crates/python/src/distance_matrix.rs
@@ -17,6 +17,17 @@ impl PyDistanceMatrix {
     }
 }
 
+#[pymethods]
+impl PyDistanceMatrix {
+    pub fn get(&self, u: usize, v: usize) -> f32 {
+        self.distance_matrix[[u, v]]
+    }
+
+    pub fn set(&mut self, u: usize, v: usize, d: f32) {
+        self.distance_matrix[[u, v]] = d
+    }
+}
+
 pub fn register(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyDistanceMatrix>()?;
     Ok(())
