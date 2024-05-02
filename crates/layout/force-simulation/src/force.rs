010aacfc02f46582aa2e127d5834d92fb44a827b
Yosuke Onoue <onoue@likr-lab.com>
2024-05-02 19:02:05 +0900

    feat: remove unmaintained crates

--- crates/layout/force-simulation/src/force.rs
+++
@@ -1,9 +0,0 @@
-use crate::Point;
-
-pub trait Force {
-    fn apply(&self, points: &mut [Point], alpha: f32);
-}
-
-pub trait ForceToNode {
-    fn apply_to_node(&self, u: usize, points: &mut [Point], alpha: f32);
-}
---
+++ crates/wasm/src/layout.rs
@@ -0,0 +1,4 @@
+pub mod kamada_kawai;
+pub mod mds;
+pub mod sgd;
+pub mod stress_majorization;
