c1a9c7d79e8c6716afa085d317b7d584a0047d53
Yosuke Onoue <onoue@likr-lab.com>
2024-12-28 13:27:00 +0900

    chore(js/examples): update SGD example

--- js/examples/src/pages/ExampleSgd.jsx
+++ js/examples/src/pages/ExampleSgd.jsx
@@ -3,54 +3,58 @@ import * as d3 from "d3";
 import {
   Graph,
   DrawingEuclidean2d as Drawing,
-  SparseSgd as Sgd,
+  FullSgd as Sgd,
   Rng,
 } from "egraph/dist/web/egraph_wasm";
-import data from "egraph-dataset/USpowerGrid.json";
 import { Wrapper } from "../wrapper";
 
 export function ExampleSgd() {
   const rendererRef = useRef();
 
   useEffect(() => {
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-    const graph = new Graph();
-    const indices = new Map();
-    for (const node of data.nodes) {
-      node.fillColor = color(node.group);
-      indices.set(node.id, graph.addNode(node));
-    }
-    for (const link of data.links) {
-      const { source, target } = link;
-      graph.addEdge(indices.get(source), indices.get(target), link);
-    }
+    (async () => {
+      const response = await fetch("/data/miserables.json");
+      const data = await response.json();
 
-    const rng = Rng.seedFrom(1n);
-    const drawing = Drawing.initialPlacement(graph);
-    const sgd = new Sgd(graph, () => 20, 200, rng);
-    const scheduler = sgd.scheduler(100, 0.1);
-
-    const draw = () => {
-      if (!rendererRef.current || scheduler.isFinished()) {
-        return;
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
+      const graph = new Graph();
+      const indices = new Map();
+      for (const node of data.nodes) {
+        node.fillColor = color(node.group);
+        indices.set(node.id, graph.addNode(node));
       }
-      scheduler.step((eta) => {
-        sgd.shuffle(rng);
-        sgd.applyWithDrawingEuclidean2d(drawing, eta);
-      });
-      drawing.centralize();
-      for (const u of graph.nodeIndices()) {
-        const node = graph.nodeWeight(u);
-        node.x = drawing.x(u);
-        node.y = drawing.y(u);
+      for (const link of data.links) {
+        const { source, target } = link;
+        graph.addEdge(indices.get(source), indices.get(target), link);
       }
-      rendererRef.current.update();
-      window.requestAnimationFrame(draw);
-    };
 
-    rendererRef.current.load(data);
-    rendererRef.current.focus(0, 0);
-    draw();
+      const rng = Rng.seedFrom(1n);
+      const drawing = Drawing.initialPlacement(graph);
+      const sgd = new Sgd(graph, () => 100);
+      const scheduler = sgd.scheduler(100, 0.1);
+
+      const draw = () => {
+        if (!rendererRef.current || scheduler.isFinished()) {
+          return;
+        }
+        scheduler.step((eta) => {
+          sgd.shuffle(rng);
+          sgd.applyWithDrawingEuclidean2d(drawing, eta);
+        });
+        drawing.centralize();
+        for (const u of graph.nodeIndices()) {
+          const node = graph.nodeWeight(u);
+          node.x = drawing.x(u);
+          node.y = drawing.y(u);
+        }
+        rendererRef.current.update();
+        window.requestAnimationFrame(draw);
+      };
+
+      rendererRef.current.load(data);
+      rendererRef.current.focus(0, 0);
+      draw();
+    })();
   }, []);
 
   return (
@@ -62,10 +66,10 @@ export function ExampleSgd() {
     >
       <eg-renderer
         ref={rendererRef}
-        default-node-width="5"
-        default-node-height="5"
+        default-node-width="10"
+        default-node-height="10"
         default-node-stroke-color="#fff"
-        default-node-stroke-width="1"
+        default-node-stroke-width="1.5"
         default-node-type="circle"
         default-link-stroke-color="#999"
         default-link-stroke-opacity="0.6"
